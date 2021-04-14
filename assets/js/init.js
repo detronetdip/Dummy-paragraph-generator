7582fa9659b30c19098a132c1c56f1a252ff1bdf
Ayondip Jana <64276906+dazal-32@users.noreply.github.com>
2021-04-14 09:51:13 +0530

    Create init.js

---
+++ assets/js/init.js
@@ -0,0 +1,7 @@
+$("NULL").init($("NULL").randomNuml());
+document.getElementById("ep").innerHTML = string;
+function regerate() {
+    string='';
+  $("NULL").init($("NULL").randomNuml());
+  document.getElementById("ep").innerHTML = string;
+}
